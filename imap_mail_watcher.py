7e0b69d7b75231277dbf5703201c32078788e4b6
shiva768 <reika.github@zelretch.com>
2018-07-06 18:31:41 +0900

    rename

--- imap_mail_watcher.py
+++ imap_mail_watcher.py
@@ -1,95 +1,41 @@
-import imaplib
-import re
-from logging import getLogger
-from queue import Queue
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
 
-from imap_push_receiver import PushReceiver
-from mail_parser import MailParser
+from asyncio import get_event_loop
+from logging import DEBUG, Formatter, StreamHandler, getLogger
+
+from mail_watcher import MailWatcher
+from mattermost_client import MattermostClient
+from setting_manager import SettingManager
 
 """ logger setting """
-LOGGER = getLogger('imap-mail-watcher').getChild('watcher')
+LOGGER = getLogger('imap-mail-watcher')
+LOGGER.setLevel(DEBUG)
+handler = StreamHandler()
+handler.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+LOGGER.addHandler(handler)
 """ /logger setting """
 
-UID_REGEX = re.compile(b'UID ([0-9]+)')
+
+def main():
+    setting = SettingManager()
+    loop = get_event_loop()
+
+    def stop():
+        loop.stop()
+
+    for user in setting.users:
+        loop.run_until_complete(parallel_process(user, setting.common, stop))
 
 
-class MailWatcher:
+async def parallel_process(user, common, stop):
+    client = MattermostClient(common['mattermost'], user['mattermost']['token'])
+    watcher = MailWatcher(user, client, stop)
+    watcher.watch()
 
-    def __init__(self, user, mattermost, stop):
-        self.imap_setting = user['imap']
-        self.imap = imaplib.IMAP4_SSL(self.imap_setting['host'])
-        self.imap.login(self.imap_setting['user'], self.imap_setting['password'])
-        self.mattermost = mattermost
-        status, seq_no = self.imap.select()
-        if status != 'OK':
-            raise Exception('response error')
-        self.current_uid = self.__fetch_uid(seq_no[0])
-        self.receiver = None
-        self.queue = Queue(20)
-        self.stop = stop
 
-    def __initialize_fetch(self):
-        pass
+if __name__ == '__main__':
+    LOGGER.info('*** 開始 ***')
 
-    def __fetch_uid(self, sequence_no):
-        status, response = self.imap.fetch(sequence_no, 'uid')
-        if status == 'NO':
-            return None
-        uid = self.__extract_uid_by_response(response)
-        if uid is not None:
-            LOGGER.info("fetched uid: {}".format(uid))
-            return uid
-        raise Exception('response error')
-
-    def __extract_uid_by_response(self, response):
-        for message in response:
-            LOGGER.debug("fetched uid message: {}".format(message))
-            return MailWatcher.__extract_uid_by_string(message)
-        return None
-
-    @staticmethod
-    def __extract_uid_by_string(message):
-        LOGGER.debug("extract uid message: {}".format(message))
-        matcher = UID_REGEX.search(message)
-        if matcher is not None:
-            return matcher.group(1)
-        return None
-
-    def watch(self):
-        def callback():
-            LOGGER.debug("current uid:{}".format(self.current_uid))
-            status, data = self.imap.uid('fetch', "{}:*".format(self.current_uid.decode('utf-8')), '(RFC822)')
-            LOGGER.debug("fetch status: {}, data: {}".format(status, data))
-            if status == 'NO':
-                return
-            self.__extract(data)
-
-        self.receiver = PushReceiver(self.imap_setting, callback)
-        try:
-            self.__watch()
-        except KeyboardInterrupt:
-            LOGGER.info("interrupt")
-            self.stop()
-
-    def __extract(self, data):
-        for idx, d in enumerate(data):
-            if type(d) == bytes:
-                uid = MailWatcher.__extract_uid_by_string(d)
-                if uid is None or uid == self.current_uid:
-                    continue
-                self.current_uid = uid
-                message = data[idx - 1]
-                mail = MailParser(uid, message[1]).mail_parse()
-                self.mattermost.post(mail)
-
-    def __watch(self):
-        while True:
-            self.receiver.listen()
-
-    def __del__(self):
-        LOGGER.info("MailWatcher end")
-        try:
-            self.imap.close()
-            self.imap.logout()
-        except:
-            pass
+    main()
+    LOGGER.info('*** 終了 ***')
