c654139da6a4178c8c45f82d40dd0022dd8ccb44
shiva768 <reika.github@zelretch.com>
2018-07-06 21:36:23 +0900

    振り分け処理実装中

--- mattermost_client.py
+++ mattermost_client.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from logging import getLogger
 
 from mattermostdriver import Driver
@@ -8,7 +9,7 @@ LOGGER = getLogger('imap-mail-watcher').getChild('mattermost')
 
 
 class MattermostClient:
-    def __init__(self, mattermost_setting, token):
+    def __init__(self, mattermost_setting, token, distribute):
         self.driver = Driver({
             'url': mattermost_setting['url'],
             'token': token,
@@ -17,9 +18,48 @@ class MattermostClient:
             'basepath': mattermost_setting['basepath'],
             'timeout': mattermost_setting['timeout']
         })
+        self.driver.login()
+        self.distribute = distribute
 
     def post(self, mail):
-        LOGGER.info(mail)
+        channel = self.__distributing(mail)
+        LOGGER.info("{}, {}".format(channel, mail))
+
+    def __distributing(self, mail):
+        if 'drops' in self.distribute:
+            for _filter in self.distribute['drops']:
+                if self.__common_distributing(mail, _filter):
+                    return 'drops'
+
+        if 'catches' in self.distribute:
+            for _filter_set in self.distribute['catches']:
+                channel_name = _filter_set['channel_name']
+                for _filter in _filter_set['rule']:
+                    if self.__common_distributing(mail, _filter):
+                        return channel_name
+
+    def __common_distributing(self, mail, _filter):
+        pattern = self.Pattern(_filter['pattern']) if 'pattern' in _filter else self.Pattern.MATCH
+        conditions = _filter['condition']
+        for key in conditions:
+            if pattern.match(conditions[key], self.__get_property(mail, key)):
+                return True
+        return False
+
+    def __get_property(self, mail, name):
+        _name = 'mail_' + name if name in ['to', 'from'] else name
+        if _name == 'any':
+            return list(mail.__dict__.values())
+        return [getattr(mail, _name)]
+
+    class Pattern(Enum):
+        MATCH = 'match'
+        SEARCH = 'search'
+
+        def match(self, condtion: str, values: list):
+            if self == self.MATCH:
+                return condtion in values
+            return [v.find(condtion) for v in values] > 0
 
     # def api_process(yml):
     #     MATTERMOST_DRIVER.login()
